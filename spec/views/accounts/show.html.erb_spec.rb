13fae5b2b5daaecf9b1db416ab722482eb756478
Dalton Huynh <daltonh@cs.washington.edu>
2011-05-06 17:25:48 -0400

    updated rows to use index instead of id

--- spec/views/accounts/show.html.erb_spec.rb
+++ spec/views/accounts/show.html.erb_spec.rb
@@ -7,8 +7,6 @@ describe "accounts/show.html.erb" do
     
     stub = Withdrawal.new
     stub.should_receive(:created_at).and_return(Time.now)
-    stub.should_receive(:id).and_return(0)
-    stub.should_receive(:id).and_return(0)
     
     assign(:withdrawals, [stub])
     
