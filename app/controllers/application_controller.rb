7fb8c065fda4393941f932525cecc78bd105ad56
Dalton <daltonh@cs.washington.edu>
2011-05-07 09:56:01 -0400

    ensured authorization required for the whole app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :authorize
+  
+  helper_method :current_user
 
 protected
   
+  def authorize
+    redirect_to root_path unless current_user
+  end
+  
+private 
+
   def current_user
     id = session[:person_id]
     Person.find(id) if id
   end
   
-  # to prevent non-logged in users from viewing accounts
-  def authorize
-    redirect_to root_path unless current_user
-  end
 end
